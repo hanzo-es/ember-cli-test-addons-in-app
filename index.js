cf8656ed050bb77b47c602d9796df1b1c02f9613
Petr Kosikhin <mq3_storm@mail.ru>
2016-11-27 23:08:50 +0300

    initial work on addon

--- index.js
+++ index.js
@@ -1,6 +1,51 @@
 /* jshint node: true */
 'use strict';
 
+var mergeTrees    = require('broccoli-merge-trees');
+var path          = require('path');
+var existsSync    = require('exists-sync');
+var p             = require('ember-cli-preprocess-registry/preprocessors');
+var Funnel        = require('broccoli-funnel');
+
+var preprocessJs  = p.preprocessJs;
+
+
 module.exports = {
-  name: 'ember-cli-addon-tests'
+  name: 'ember-cli-addon-tests',
+
+  included(app) {
+    var addons = app.options.emberCliAddonTests;
+    this.registryAddons = app.registry.app.project.addons.filter(function(addon) {
+      return addons.indexOf(addon.name) !== -1;
+    });
+    this.appName = app.name;
+    this.appRegistry = app.registry;
+  },
+
+  generateTestTreeForAddon(addon) {
+    var treePath = path.resolve(addon.root, 'tests');
+    var preprocessedTests;
+    if (existsSync(treePath)) {
+      var tree = new Funnel(treePath, {
+        destDir: this.appName + '/tests'
+      });
+      preprocessedTests = preprocessJs(tree, '/tests', this.appName, {
+        registry: this.appRegistry
+      });
+    }
+
+    return preprocessedTests;
+  },
+
+  postprocessTree(type, tree) {
+    var self = this;
+    if (type === 'test') {
+      var includedAddonTestTrees = this.registryAddons.map(function(addon) {
+        return self.generateTestTreeForAddon(addon);
+      });
+      return mergeTrees(includedAddonTestTrees.concat(tree), {overwrite: true});
+    } else {
+      return tree;
+    }
+  }
 };
